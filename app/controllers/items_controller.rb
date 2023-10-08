17bcf002c282b7384b210464b6bc7bfdfc22d585
ayumi0511 <ayumi.tokoeda@gmail.com>
2023-10-08 14:42:54 +0900

    商品情報編集機能　修正#2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit]
-  
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -23,7 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item, notice: '商品情報が更新されました。'
     else
